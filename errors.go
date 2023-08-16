daad5204ff545dd6dd518c1a438749f8feb15c60
r3d5un <r3d5un@pm.me>
2023-08-16 09:55:44 +0200

    Create initial version of colStats

---
+++ errors.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"errors"
+)
+
+var (
+	ErrNotNumber        = errors.New("Data is not numeric")
+	ErrInvalidColumn    = errors.New("Invalid column number")
+	ErrNoFiles          = errors.New("No input files")
+	ErrInvalidOperation = errors.New("Invalid operation")
+)
